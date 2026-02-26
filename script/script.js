8c24ccb0916e72993341b578fa5a99063e4dca69
Rita Vargas <ritavargas.v@gmail.com>
2026-02-26 02:41:13 -0300

    Cambios visuales, se agrega boton funcional para borrar tareas

--- script/script.js
+++ script/script.js
@@ -1,42 +1,56 @@
 /* Agregar elementos a Tareas por hacer*/
 
-const inputTarea = document.getElementById("inputTarea")
-const porHacer = document.getElementById("porHacer")
+const inputTarea = document.getElementById("inputTarea");
+const porHacer = document.getElementById("porHacer");
 
 cargarTareas();
 
 function crearTarea(texto) {
-    let li = document.createElement("li");
-    li.textContent = texto;
-    porHacer.appendChild(li);
-}
+  let li = document.createElement("li");
+  li.textContent=(texto)
 
-
-function guardarTarea () {
-    const tarea = inputTarea.value.trim();
-    if (tarea) {
-        crearTarea(tarea);
-        inputTarea.value = ``;
-        tareaLocal();
-    }
-}
-
-function tareaLocal () {
-    let tareas = [];
-    porHacer.querySelectorAll(`li`).forEach(function (item) {
-        tareas.push(item.textContent.trim());
+  const borrarBoton = document.createElement(`button`);
+  borrarBoton.type = "button";
+  borrarBoton.textContent = `Borrar`;
+  borrarBoton.className = `btn-borrar`;
+  
+  
+  borrarBoton.addEventListener(`click`, () => {
+      li.remove();
+      tareaLocal();
     });
-    localStorage.setItem(`tareas`, JSON.stringify(tareas));
+    porHacer.appendChild(li);
+    li.appendChild(borrarBoton);
+}
+
+function guardarTarea() {
+  const tarea = inputTarea.value.trim();
+  if (tarea) {
+    crearTarea(tarea);
+    inputTarea.value = ``;
+    tareaLocal();
+  }
+}
+
+function tareaLocal() {
+  const tareas = [];
+  porHacer.querySelectorAll(`li`).forEach((item) => {
+    tareas.push(item.textContent.trim());
+  });
+  localStorage.setItem(`tareas`, JSON.stringify(tareas));
 }
 
 function cargarTareas() {
-    const tareas = JSON.parse(localStorage.getItem(`tareas`)) || [];
-    tareas.forEach(crearTarea)
-};
+  const tareas = JSON.parse(localStorage.getItem(`tareas`)) || [];
+  tareas.forEach(crearTarea);
+}
 
-inputTarea.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        document.getElementById("btnGuardar").click();
-    }
-})
+const btnGuardar = document.getElementById("btnGuardar")
+btnGuardar.addEventListener(`click`, guardarTarea)
+
+inputTarea.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    document.getElementById("btnGuardar").click();
+  }
+});
